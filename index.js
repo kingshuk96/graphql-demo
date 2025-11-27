84a9ed670cfa8db2a8312fe7f402634f2a3a5d81
kingshuk96 <sanyalbittu96@gmail.com>
2025-11-27 18:32:41 +0530

    initial setup

--- index.js
+++ index.js
@@ -1,19 +1,36 @@
-var { graphql, buildSchema } = require("graphql")
- 
-var schema = buildSchema(`
+import { graphql, buildSchema } from "graphql"
+import express from "express"
+import { createHandler } from "graphql-http/lib/use/express";
+import { ruruHTML } from "ruru/server";
+const schema = buildSchema(`
   type Query {
     hello: String
     age: Int
   }
 `)
- 
-var rootValue = { 
-    hello: () => "Hello world!",
-     age: () => 42
+
+const rootValue = {
+  hello: () => "Hello world!",
+  age: () => 42
 }
- 
-var source = "{ age }"
- 
+
+const source = "{ age }"
+
 graphql({ schema, source, rootValue }).then(response => {
   console.log(response)
+})
+
+const app = express();
+
+app.all('/graphql', createHandler({ schema, rootValue }));
+
+app.get('/', (req, res) => {
+  res.type('html');
+  res.end(ruruHTML({
+    title: 'GraphQL',
+    description: 'GraphQL'
+  }))
+})
+app.listen(4000, () => {
+  console.log('Listening on port 4000')
 })
